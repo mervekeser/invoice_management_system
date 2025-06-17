f57c08bb4e21e0995d815ce5e17de929b23aa6ee
mervekeser <mervekeser8@outlook.com>
2025-06-17 21:15:56 +0300

    project setup

---
+++ src/main/java/dev/mervekeser/invoice_management_system/InvoiceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.mervekeser.invoice_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mervekeser/invoice_management_system/InvoiceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mervekeser.invoice_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

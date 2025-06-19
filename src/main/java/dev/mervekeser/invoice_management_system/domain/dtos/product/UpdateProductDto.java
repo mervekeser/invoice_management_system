e693c786194ae1487bf6ca99f849feb7d0fd158e
mervekeser <mervekeser8@outlook.com>
2025-06-19 17:25:04 +0300

    create product dtos

---
+++ src/main/java/dev/mervekeser/invoice_management_system/domain/dtos/product/UpdateProductDto.java
@@ -0,0 +1,15 @@
+package dev.mervekeser.invoice_management_system.domain.dtos.product;
+
+import jakarta.validation.constraints.*;
+
+public record UpdateProductDto(
+        @NotNull @NotBlank @Size(max = 255)
+        String description,
+        @NotNull @PositiveOrZero
+        int quantity,
+        @NotNull @Positive
+        double unitPrice,
+        @NotNull @Positive
+        double taxRate
+) {
+}

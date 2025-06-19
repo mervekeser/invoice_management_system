ca8b7761d0f3656cdfd8d92622ecfbb7afced721
mervekeser <mervekeser8@outlook.com>
2025-06-19 18:32:43 +0300

    create payment mapper

---
+++ src/main/java/dev/mervekeser/invoice_management_system/mappers/PaymentMapper.java
@@ -0,0 +1,13 @@
+package dev.mervekeser.invoice_management_system.mappers;
+
+import dev.mervekeser.invoice_management_system.domain.dtos.payment.CreatePaymentDto;
+import dev.mervekeser.invoice_management_system.domain.dtos.payment.PaymentResponseDto;
+import dev.mervekeser.invoice_management_system.domain.entities.Payment;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PaymentMapper {
+    PaymentResponseDto toDto(Payment payment);
+    Payment toEntity(CreatePaymentDto createPaymentDto);
+}

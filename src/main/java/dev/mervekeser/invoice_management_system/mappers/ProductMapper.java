ba6eb8b51e12f4a2759e5d71cdc05e95889ebeb4
mervekeser <mervekeser8@outlook.com>
2025-06-19 18:33:46 +0300

    create product mapper

---
+++ src/main/java/dev/mervekeser/invoice_management_system/mappers/ProductMapper.java
@@ -0,0 +1,13 @@
+package dev.mervekeser.invoice_management_system.mappers;
+
+import dev.mervekeser.invoice_management_system.domain.dtos.product.CreateProductDto;
+import dev.mervekeser.invoice_management_system.domain.dtos.product.ProductResponseDto;
+import dev.mervekeser.invoice_management_system.domain.entities.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ProductMapper {
+    ProductResponseDto toDto(Product product);
+    Product toEntity(CreateProductDto createProductDto);
+}

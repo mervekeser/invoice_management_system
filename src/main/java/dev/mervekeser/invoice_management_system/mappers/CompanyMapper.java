ce3eae4455570579cb0264fa65ec7337e2d4de58
mervekeser <mervekeser8@outlook.com>
2025-06-19 17:40:49 +0300

    create company mapper

---
+++ src/main/java/dev/mervekeser/invoice_management_system/mappers/CompanyMapper.java
@@ -0,0 +1,13 @@
+package dev.mervekeser.invoice_management_system.mappers;
+
+import dev.mervekeser.invoice_management_system.domain.dtos.company.CompanyResponseDto;
+import dev.mervekeser.invoice_management_system.domain.dtos.company.CreateCompanyDto;
+import dev.mervekeser.invoice_management_system.domain.entities.Company;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CompanyMapper {
+    CompanyResponseDto toDto(Company company);
+    Company toEntity(CreateCompanyDto createCompanyDto);
+}

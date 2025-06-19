e0dfd152690b3e7a2213f977a7d049b6ee41696c
mervekeser <mervekeser8@outlook.com>
2025-06-19 14:59:32 +0300

    add validation address dtos

--- src/main/java/dev/mervekeser/invoice_management_system/domain/dtos/address/CreateAddressDto.java
+++ src/main/java/dev/mervekeser/invoice_management_system/domain/dtos/address/CreateAddressDto.java
@@ -1,8 +1,19 @@
 package dev.mervekeser.invoice_management_system.domain.dtos.address;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record CreateAddressDto(
+        @Size(max = 255)
         String content,
+        @NotBlank
+        @NotNull
+        @Size(max = 25)
         String city,
+        @NotBlank
+        @NotNull
+        @Size(max = 25 )
         String district
 ) {
 }

1400de43a30693618d71f9a5f6dc7d4da93d98b4
mervekeser <mervekeser8@outlook.com>
2025-06-19 18:30:53 +0300

    create invoice mapper

---
+++ src/main/java/dev/mervekeser/invoice_management_system/mappers/InvoiceMapper.java
@@ -0,0 +1,13 @@
+package dev.mervekeser.invoice_management_system.mappers;
+
+import dev.mervekeser.invoice_management_system.domain.dtos.invoice.CreateInvoiceDto;
+import dev.mervekeser.invoice_management_system.domain.dtos.invoice.InvoiceResponseDto;
+import dev.mervekeser.invoice_management_system.domain.entities.Invoice;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface InvoiceMapper {
+    InvoiceResponseDto toDto(Invoice invoice);
+    Invoice toEntity(CreateInvoiceDto createInvoiceDto);
+}

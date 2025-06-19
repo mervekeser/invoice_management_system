b5d91885f07adf5518d8983bb0e29c5fb7d46fc0
mervekeser <mervekeser8@outlook.com>
2025-06-19 17:37:55 +0300

    create address mapper

---
+++ src/main/java/dev/mervekeser/invoice_management_system/mappers/AddressMapper.java
@@ -0,0 +1,13 @@
+package dev.mervekeser.invoice_management_system.mappers;
+
+import dev.mervekeser.invoice_management_system.domain.dtos.address.AddressResponseDto;
+import dev.mervekeser.invoice_management_system.domain.dtos.address.CreateAddressDto;
+import dev.mervekeser.invoice_management_system.domain.entities.Address;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface AddressMapper {
+    AddressResponseDto toDto(Address address);
+    Address toEntity(CreateAddressDto createAddressDto);
+}

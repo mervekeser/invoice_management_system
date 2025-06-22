90184ef2cf7a66d749ffb1fcd2bae954add35918
mervekeser <mervekeser8@outlook.com>
2025-06-22 15:25:29 +0300

    fix invoice status and payment method enums

--- src/main/java/dev/mervekeser/invoice_management_system/domain/entities/enums/InvoiceStatus.java
+++ src/main/java/dev/mervekeser/invoice_management_system/domain/entities/enums/InvoiceStatus.java
@@ -1,8 +1,17 @@
 package dev.mervekeser.invoice_management_system.domain.entities.enums;
 
 public enum InvoiceStatus {
-    DRAFT,
-    SENT,
-    PAID,
-    DELAYED
+    DRAFT("Draft"),
+    SENT("Sent"),
+    PAID("Paid"),
+    DELAYED("Delayed");
+
+    private final String status;
+    InvoiceStatus(String status){
+        this.status = status;
+    }
+
+    public String getInvoiceMethod(){
+        return status;
+    }
 }
--- src/main/java/dev/mervekeser/invoice_management_system/domain/entities/enums/PaymentMethod.java
+++ src/main/java/dev/mervekeser/invoice_management_system/domain/entities/enums/PaymentMethod.java
@@ -1,7 +1,16 @@
 package dev.mervekeser.invoice_management_system.domain.entities.enums;
 
 public enum PaymentMethod {
-    CASH,
-    CREDIT_CARD,
-    TRANSFER
+    CASH("Cash"),
+    CREDIT_CARD("Credit Card"),
+    TRANSFER("Transfer");
+
+    private final String method;
+    PaymentMethod(String method){
+        this.method = method;
+    }
+
+    public String getPaymentMethod(){
+        return method;
+    }
 }
